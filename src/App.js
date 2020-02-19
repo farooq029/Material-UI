f47c1320c576532f2a8e3b7f62597c1fd7bfb12a
farooq029 <farooqhayat12@gmail.com>
2020-02-19 12:25:25 +0000

    Pages for three division

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Body from "./components/Body";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      
+    <Header />
+    <Body/>
+    <Footer />
+
     </div>
   );
 }
